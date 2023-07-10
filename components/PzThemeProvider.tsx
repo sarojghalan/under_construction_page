41eb09629a2e7587e8cde96bc57552424c5356f8
Hypekage <jmestmnag@gmail.com>
2023-07-11 00:19:26 +0545

    Creating Global Theme Provider As next js 13.4 has little issue about the dynamically changed behaviour of styled components, so to overcome that issue we need to flag "use client" at the top, and we dont want whole layout to be client side so, making a different file become better approach, where this files has been flaged with "use client" ,and only required props of styled component are flaged "use client". And global theme provider recieves children, and so on overall every component can acess global theme.

---
+++ components/PzThemeProvider.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { ThemeProvider } from "styled-components";
+import { PZTheme } from "@/lib/theme";
+
+export const PZThemeProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => <ThemeProvider theme={PZTheme}>{children}</ThemeProvider>;
---
+++ lib/theme.ts
@@ -0,0 +1,6 @@
+export const PZTheme = {
+  color: {
+    primaryColor: "#fff",
+    secondaryColor: "#000",
+  },
+};
